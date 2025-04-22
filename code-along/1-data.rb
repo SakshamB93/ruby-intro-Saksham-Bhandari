f7b2062b78f9f52112bdd0e19e31561ebedb38c3
Saksham Bhandari <saksham.bhandari@kellogg.northwestern.edu>
2025-04-22 14:32:52 +0000

    Worked on code along db 1 and 2

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -6,14 +6,42 @@
 
 # Numbers
 
+# puts 3
+# puts 5
+# puts 3+5
+# puts 5-3
+# puts 5*3
+# puts 5/3.0
+
+# it does multiplication, subtractio, division like it does addition above
+
 # Non-numbers
 
+# puts "zeros and ones"
+
 # True, False
+
+# puts true
+# puts false
 
 # Nothing
 
 # Variables
 
+# x=3
+# y=5
+# puts x*x+y
+
+# z=x+y
+# puts z
+
 # Combine strings and variables
 
+# puts "tacos on today's menu: #{x*z}"
+college="Kellogg"
+
+puts "#{college} School of Management"
+
 # String manipulation
+
+puts college.upcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,11 +4,41 @@
 
 # Booleans
 
+true
+false
+
 # Boolean Expressions
+
+x=3
+y=5
+puts x==y
+puts x!=y
+puts x>y
+puts x<y
+puts x>=y
+puts x<=y
+
+# to check for if something is eqaul to other, we use "=="
+# to check for if something is not eqaul to other, we use "!="
+
 
 # If Conditional Logic
 
+# for OR we use "||"
+# for AND we use "&&"
+
+if x!=y && x<4
+    puts "OMG!!"
+elsif x<y
+    puts "Yayyieee"
+else
+    puts "Whatever"
+end
+
 # If/Else Conditional Logic
+
+
+
 
 # Elsif Conditional Logic
 
